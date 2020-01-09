d378c1b825e4e1adcf771d82a82aeb4158ca0971
Mahabub <mahabub775@gmail.com>
2020-01-09 22:10:48 +0600

    modify contoller for try cath issue

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -30,15 +30,30 @@ namespace WebApi_ADO_DotNet.Controllers
         public async Task<ActionResult<IEnumerable<Customer>>> Gets()
         {
             List<Customer> _oCustomers = new List<Customer>();
-            Customer oCustomer = new Customer();
-            _oCustomers = Customer.Gets();
+            try
+            {
+                _oCustomers = Customer.Gets();
+            }
+            catch (Exception e)
+            {
+                Customer oCustomer = new Customer();
+                oCustomer.Message = e.Message;
+                _oCustomers.Add(oCustomer);
+            }
             return _oCustomers.ToList();
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Customer>> Get(int id)
         {
             _oCustomer = new Customer();
-            _oCustomer = Customer.Get(id);
+            try
+            {
+                _oCustomer = Customer.Get(id);
+            }
+            catch (Exception e)
+            {
+                _oCustomer.Message = e.Message;
+            }
             return _oCustomer;
         }
 
@@ -46,7 +61,14 @@ namespace WebApi_ADO_DotNet.Controllers
         public async Task<ActionResult<Customer>> Save(Customer oCustomer)
         {
             _oCustomer = new Customer();
-            _oCustomer = oCustomer.Save(oCustomer);
+            try
+            {
+                _oCustomer = oCustomer.Save(oCustomer);
+            }
+            catch (Exception e)
+            {
+                _oCustomer.Message = e.Message;
+            }
             return _oCustomer;
         }
 
@@ -55,7 +77,14 @@ namespace WebApi_ADO_DotNet.Controllers
         {
             
             _oCustomer = new Customer();
-            _oCustomer.Message = _oCustomer.Delete(id);
+            try
+            {
+                _oCustomer.Message = _oCustomer.Delete(id);
+            }
+            catch (Exception e)
+            {
+                _oCustomer.Message = e.Message;
+            }
             return _oCustomer;
         }
 
@@ -64,8 +93,15 @@ namespace WebApi_ADO_DotNet.Controllers
         public async Task<ActionResult<Customer>> GetCustomerIncomeInfo()
         {
             _oCustomer = new Customer();
-            _oCustomer.SectionWiseInComeList = Customer.Gets("SELECT 0 AS CustomerID, '' AS Name, '' AS Profession, 0 As Latitude, 0 As Longitude,  0 As EducatonLevel, Section,  SUM(MonthlyIncome) As MonthlyIncome FROM Customer  Group by Section  Order by Section");
-            _oCustomer.EducationLavelWiseInComeList = Customer.Gets("SELECT 0 AS CustomerID, '' AS Name, '' AS Profession, 0 As Latitude, 0 As Longitude, 0 As Section, EducatonLevel,  SUM(MonthlyIncome) As MonthlyIncome FROM Customer A Group by EducatonLevel  Order by EducatonLevel");
+            try
+            {
+                _oCustomer.SectionWiseInComeList = Customer.Gets("SELECT 0 AS CustomerID, '' AS Name, '' AS Profession, 0 As Latitude, 0 As Longitude,  0 As EducatonLevel, Section,  SUM(MonthlyIncome) As MonthlyIncome FROM Customer  Group by Section  Order by Section");
+                _oCustomer.EducationLavelWiseInComeList = Customer.Gets("SELECT 0 AS CustomerID, '' AS Name, '' AS Profession, 0 As Latitude, 0 As Longitude, 0 As Section, EducatonLevel,  SUM(MonthlyIncome) As MonthlyIncome FROM Customer A Group by EducatonLevel  Order by EducatonLevel");
+            }
+            catch (Exception e)
+            {
+                _oCustomer.Message = e.Message;
+            }
             return _oCustomer;
         }
 
@@ -73,12 +109,10 @@ namespace WebApi_ADO_DotNet.Controllers
         [HttpPost, DisableRequestSizeLimit]
         public ActionResult ExcelUpload()
         {
-            var sMessage = "Success";
-            var file = Request.Form.Files[0];
-              if (Request.Form.Files.Count > 0)
-                {
-
-
+            var Message = "Success";
+            try
+            {
+                var file = Request.Form.Files[0];
                 var folderName = Path.Combine("Resources", "TempFile");
                 var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
 
@@ -91,7 +125,7 @@ namespace WebApi_ADO_DotNet.Controllers
 
                     using (var stream = new FileStream(fullPath, FileMode.Create))
                     {
-                        file.CopyTo(stream);
+                        file.CopyTo(stream); //Copy to directory 
 
                         if (file.FileName.EndsWith(".xls"))
                         {
@@ -120,17 +154,20 @@ namespace WebApi_ADO_DotNet.Controllers
                             oCustomer.Longitude = Convert.ToDouble(finalRecords.Rows[i][6]);
                             _oCustomer = oCustomer.Save(oCustomer);
                         }
-
-
                     }
-                    return Ok(new { sMessage });
+                    return Ok(new { Message });
                 }
                 else
                 {
-                    return BadRequest();
+                    Message = "Sorry, There is No File";
+                    return Ok(new { Message });
                 }
             }
-            return Ok(new { sMessage });
+            catch (Exception e)
+            {
+                Message = e.Message;
+            }
+            return Ok(new { Message });
         }
 
         public int EducationLavel(string sEducationLavel)
